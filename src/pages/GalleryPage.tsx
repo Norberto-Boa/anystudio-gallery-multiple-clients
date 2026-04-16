43c956577f2117d83bb44871563a74fa31bffb7e
Norberto Boa <nboa.26@gmail.com>
2026-04-16 02:18:16 +0200

    feat: add metatags to galleryPage

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 import type { DriveItem } from "../types/drive";
 import {
   getDriveGridImage,
+  getDriveGridImageHuge,
   getDriveItem,
   listFolderContents,
 } from "../services/drive";
 import { ImagePreviewModal } from "../components/ImagePreviewModal";
 import { GalleryHeroHeader } from "../components/GalleryHeroHeader";
 import { formatDate } from "../helpers/format-date";
+import { Helmet } from "react-helmet-async";
 
 export function GalleryPage() {
   const { folderId = "" } = useParams();
@@ -19,6 +21,7 @@ export function GalleryPage() {
     null,
   );
   const [currentFolder, setCurrentFolder] = useState<DriveItem | null>(null);
+  const url = window.location.href;
 
   useEffect(() => {
     async function load() {
@@ -76,67 +79,108 @@ export function GalleryPage() {
   if (loading) return <div>Carregando galeria...</div>;
 
   return (
-    <div className="mx-auto max-w-7xl p-6">
-      {images.length > 0 ? (
-        <GalleryHeroHeader
-          title={currentFolder?.name || "Um lindo album"}
-          coverUrl={images[0].thumbnailLink}
-          date={formatDate(currentFolder?.createdTime)}
+    <>
+      <Helmet>
+        <title>
+          {currentFolder?.name ?? "Lindas Fotos"} | Anystudio Gallery
+        </title>
+
+        <meta
+          name="description"
+          content="Momentos Registados durante a sua cerimonia, obrigado pela sua confianca!"
         />
-      ) : (
-        <h1 className="mb-6 text-3xl font-bold">Galeria</h1>
-      )}
 
-      {folders.length > 0 && (
-        <>
-          <h2 className="mb-4 text-xl font-semibold">Subpastas</h2>
-          <div className="mb-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            {folders.map((folder) => (
-              <a
-                key={folder.id}
-                href={`/gallery/${folder.id}`}
-                className="rounded-2xl border p-4 shadow-sm"
-              >
-                <div className="mb-3 aspect-4/3 rounded-xl bg-neutral-100" />
-                <h3 className="font-medium">{folder.name}</h3>
-              </a>
-            ))}
-          </div>
-        </>
-      )}
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={currentFolder?.name} />
+        <meta
+          property="og:description"
+          content={
+            "Momentos Registados durante a sua cerimonia, obrigado pela sua confianca!"
+          }
+        />
+        <meta
+          property="og:image"
+          content={getDriveGridImageHuge(images[0].thumbnailLink)}
+        />
+        <meta property="og:url" content={url} />
 
-      {images.length > 0 && (
-        <>
-          <h2 className="mb-4 text-xl font-semibold">Imagens</h2>
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={currentFolder?.name} />
+        <meta
+          name="twitter:description"
+          content={
+            "Momentos Registados durante a sua cerimonia, obrigado pela sua confianca!"
+          }
+        />
+        <meta
+          name="twitter:image"
+          content={getDriveGridImageHuge(images[0].thumbnailLink)}
+        />
+      </Helmet>
+      <div className="mx-auto max-w-7xl p-6">
+        {images.length > 0 ? (
+          <GalleryHeroHeader
+            title={currentFolder?.name || "Um lindo album"}
+            coverUrl={images[0].thumbnailLink}
+            date={formatDate(currentFolder?.createdTime)}
+          />
+        ) : (
+          <h1 className="mb-6 text-3xl font-bold">Galeria</h1>
+        )}
 
-          <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-            {images.map((image, index) => (
-              <button
-                onClick={() => handleOpenImage(index)}
-                type="button"
-                key={image.id}
-                className="mb-4 break-inside-avoid overflow-hidden rounded-2xl bg-neutral-100 shadow-sm transition hover:shadow-md"
-              >
-                <img
-                  src={getDriveGridImage(image.thumbnailLink)}
-                  alt={image.name}
-                  className="h-auto w-full object-cover transition duration-300 hover:scale-[1.02]"
-                  loading="lazy"
-                  referrerPolicy="no-referrer"
-                />
-              </button>
-            ))}
-          </div>
-        </>
-      )}
+        {folders.length > 0 && (
+          <>
+            <h2 className="mb-4 text-xl font-semibold">Subpastas</h2>
+            <div className="mb-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+              {folders.map((folder) => (
+                <a
+                  key={folder.id}
+                  href={`/gallery/${folder.id}`}
+                  className="rounded-2xl border p-4 shadow-sm"
+                >
+                  <div className="mb-3 aspect-4/3 rounded-xl bg-neutral-100" />
+                  <h3 className="font-medium">{folder.name}</h3>
+                </a>
+              ))}
+            </div>
+          </>
+        )}
 
-      <ImagePreviewModal
-        images={images}
-        currentIndex={currentImageIndex}
-        onClose={handleCloseModal}
-        onPrev={handlePrevImage}
-        onNext={handleNextImage}
-      />
-    </div>
+        {images.length > 0 && (
+          <>
+            <h2 className="mb-4 text-xl font-semibold">Imagens</h2>
+
+            <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
+              {images.map((image, index) => (
+                <button
+                  onClick={() => handleOpenImage(index)}
+                  type="button"
+                  key={image.id}
+                  className="mb-4 break-inside-avoid overflow-hidden rounded-2xl bg-neutral-100 shadow-sm transition hover:shadow-md"
+                >
+                  <img
+                    src={getDriveGridImage(image.thumbnailLink)}
+                    alt={image.name}
+                    className="h-auto w-full object-cover transition duration-300 hover:scale-[1.02]"
+                    loading="lazy"
+                    referrerPolicy="no-referrer"
+                  />
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+
+        <ImagePreviewModal
+          images={images}
+          currentIndex={currentImageIndex}
+          onClose={handleCloseModal}
+          onPrev={handlePrevImage}
+          onNext={handleNextImage}
+        />
+      </div>
+    </>
   );
 }
